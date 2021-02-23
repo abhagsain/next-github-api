8a48c2d674f39b4bd400388fd809d11f11e48dc0
Anurag Bhagsain <abhagsain@gmail.com>
2021-02-23 22:24:33 +0530

    fix: 🐞 remove typo and fix URL 🤦‍♂️

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -2,13 +2,9 @@
 import axios from "axios";
 export default async (req, res) => {
   try {
-    const { data } = await axios.get(
-      "https://github-api-ashen.vercel.app/api/jobs"
-    );
+    const { data } = await axios.get("https://jobs.github.com/positions.json");
     return res.status(200).json({ data });
   } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Oopes there was an error", error: err });
+    return res.status(500).json({ message: "Oopes there was an error", error });
   }
 };
